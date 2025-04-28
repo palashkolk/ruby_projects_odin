790e6219fb25020cfce62c375fac665380145b5c
palashkolk <203395566+palashkolk@users.noreply.github.com>
2025-04-28 16:45:12 +0530

    Adds anothet methods for the same job
    Done after seeing community solutions.

--- substrings.rb
+++ substrings.rb
@@ -1,18 +1,29 @@
 def substrings(word, array_of_substrings)
   formatted_word=word.downcase
   result_hash=Hash.new(0)
-  array_of_substrings.each do |string|
+  array_of_substrings.each do |substring|
     count=0
-    counter=formatted_word.gsub(string)
-    counter.each do
-      count+=1
-    end
-    if count>0
-      result_hash[string]+=count
-    end
+    counter=formatted_word.gsub(substring)
+    counter.each {count+=1}
+    result_hash[substring]+=count if count>0
   end
-  puts result_hash
+  result_hash
 
 end
+
+def substrings1(text, array_of_substrings)
+  words_in_text=text.split(" ")
+  array_of_substrings.reduce(Hash.new(0)) do |result_hash, substring|
+    words_in_text.each do |word|
+      if word.downcase.include?(substring.downcase)
+        result_hash[substring] += 1 
+      end
+    end
+    result_hash
+  end
+end
+
+
+
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("Howdy partner, sit down! How's it going?", dictionary)
+puts substrings1("Howdy partner, sit down! How's it going?", dictionary)
